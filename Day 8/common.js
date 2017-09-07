10a44e0f18683b1ab9c640670ce75b964331fc43
hongocnguyen1994 <hongocnguyen1994@gmail.com>
2017-09-07 17:52:26 +0700

    update day 7 day 8

---
+++ Day 8/common.js
@@ -0,0 +1,16 @@
+//alert("PHP09");
+document.getElementById("demo").innerHTML = "Hello JavaScript";
+
+document.getElementById("nameUser").innerHTML = "Hồ Ngọc Nguyên";
+document.getElementById("dayOfBirth").innerHTML = "28/03/1994";
+document.getElementById("gender").innerHTML = "Male";
+document.getElementById("nameSchool").innerHTML = "Duy Tan University";
+
+var x=7, y=5;
+document.write("x = ",x,"<br>");
+document.write("y = ",y,"<br>");
+document.write("x + y = ",x+y,"<br>");
+document.write("x - y = ",x-y,"<br>");
+document.write("x / y = ",x/y,"<br>");
+document.write("x % y = ",x%y,"<br>");
+document.write(x++ - y-- + x++ + --y + ++x"<br>");
